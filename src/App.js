c78b328a5105917ffa760a81fa7b8482bec53218
Ruzgar Zere <rzere@rollins.edu>
2021-03-09 20:32:13 +0300

    disket logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './disket.png';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
@@ -12,17 +12,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          // <p>
+          //   Edit <code>src/App.js</code> and save to reload.
+          // </p>
+          // <a
+          //   className="App-link"
+          //   href="https://reactjs.org"
+          //   target="_blank"
+          //   rel="noopener noreferrer"
+          // >
+          //   Learn React
+          // </a>
         </header>
       </div>
     );
